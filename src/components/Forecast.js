daff8b272082fbcabf23b64c07a2906ea5299aab
bkRahul <rahulsatya1068@gmail.com>
2019-06-19 15:17:08 +0530

    fixed mixed content issue

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -12,7 +12,7 @@ const Forecast = (props) => {
     <div className="forecast_item">
        { console.log(props.forecast.main.temp)}
        {props.forecast.dt_txt && <p>{getDayName(props.forecast.dt_txt.substring(0, 10))}</p>}
-      {props.forecast.weather[0].icon && <div><img src={"http://openweathermap.org/img/w/" + props.forecast.weather[0].icon + ".png"} /></div> }
+      {props.forecast.weather[0].icon && <div><img src={"https://openweathermap.org/img/w/" + props.forecast.weather[0].icon + ".png"} /></div> }
       {props.forecast.main.temp && <p>{props.forecast.main.temp}</p>}
     </div>
   );
